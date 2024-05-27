2cb71a55ad91f88b31f481b7ebf60fd215a87883
Omkar Patole <omkarpatole1799@gmail.com>
2024-05-27 08:06:26 +0530

    Middleware added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+} from "react-router-dom";
 import RootCom from "./components/RootCom/RootCom";
 import LoginPageCom from "./components/LoginPageCom/LoginPageCom";
 import DashboardCom from "./components/DashboardCom/DashboardCom";
 import SignupPageCom from "./components/SignupPageCom/SignupPageCom";
-import './App.css'
+import "./App.css";
+import StudentRegCom from "./components/StudentRegCom/StudentRegCom";
+import StudentSessionCom from "./components/StudentSessionCom/StudentSessionCom";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootCom />,
+    loader: () => {
+      if (!localStorage.getItem("token")) {
+        return redirect("/login");
+      } else {
+        return null;
+      }
+    },
     children: [
-      { path: "/login", element: <LoginPageCom /> },
       { path: "/signup", element: <SignupPageCom /> },
       { path: "/dashboard", element: <DashboardCom /> },
-      { path: "/dashboard/add-student", element:<DashboardCom/> },
+      { path: "/dashboard/add-student", element: <StudentRegCom /> },
+      { path: "/dashboard/add-session", element: <StudentSessionCom /> },
     ],
+  },
+
+  {
+    path: "/login",
+    loader: () => {
+      if (localStorage.getItem("token")) {
+        return redirect("/dashboard");
+      } else {
+        return null;
+      }
+    },
+    element: <LoginPageCom />,
   },
 ]);
 
