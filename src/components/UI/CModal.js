6988991eb709a5acde7f94b087690d991bb2ff60
omkarpatole1799 <omkarpatole1799@gmail.com>
2024-06-13 22:09:33 +0530

    BUG fix: modal was opening automatically

--- src/components/UI/CModal.js
+++ src/components/UI/CModal.js
@@ -29,9 +29,10 @@ export default function CModal({
   // );
 
   let _isModalOpen = isModalOpen(id);
+  console.log(_isModalOpen,'-here')
   return (
     <>
-      {!_isModalOpen && (
+      {_isModalOpen && (
         <ModalOverlay
           toggleModal={toggleModal}
           staticBackdrop={staticBackdrop}
@@ -40,7 +41,7 @@ export default function CModal({
 
       <div
         className={` bg-white !rounded-md z-50 transition-all duration-300 absolute shadow-xl min-h-[10rem] left-[50%] translate-x-[-50%] translate-y-[-50%] ${
-          !_isModalOpen ? `top-[50%] opacity-100 visible` : `top-[55%] opacity-0 invisible`
+          _isModalOpen ? `top-[50%] opacity-100 visible` : `top-[55%] opacity-0 invisible`
         } top-[50%] w-[80vw]`}
       >
         <ModalHeader id={id} title={title} toggleModal={toggleModal} />
