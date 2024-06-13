c5c5aae5c94a9720fd2cb303a345514e4737f61d
omkarpatole1799 <omkarpatole1799@gmail.com>
2024-06-13 19:14:11 +0530

    Modal open animation added

--- src/components/UI/CModal.js
+++ src/components/UI/CModal.js
@@ -8,23 +8,44 @@ export default function CModal({
   staticBackdrop = false,
 }) {
   const { isModalOpen, toggleModal } = useModalCtx();
+  // return (
+  //   <>
+  //     {isModalOpen(id) && (
+  //       <>
+  //         <ModalOverlay
+  //           toggleModal={toggleModal}
+  //           staticBackdrop={staticBackdrop}
+  //         />
+
+  //         <div
+  //           className={` bg-white !rounded-md z-50 transition-all duration-300 absolute shadow-xl min-h-[10rem] left-[50%] translate-x-[-50%] translate-y-[-50%] top-[50%] w-[80vw]`}
+  //         >
+  //           <ModalHeader id={id} title={title} toggleModal={toggleModal} />
+  //           <ModalBody>{children}</ModalBody>
+  //         </div>
+  //       </>
+  //     )}
+  //   </>
+  // );
+
+  let _isModalOpen = isModalOpen(id);
   return (
     <>
-      {isModalOpen(id) && (
-        <>
-          <ModalOverlay
-            toggleModal={toggleModal}
-            staticBackdrop={staticBackdrop}
-          />
-
-          <div
-            className={` bg-white z-50 absolute shadow-xl min-h-[10rem] left-[50%] translate-x-[-50%] top-[10%] w-[80vw]`}
-          >
-            <ModalHeader id={id} title={title} toggleModal={toggleModal} />
-            <ModalBody>{children}</ModalBody>
-          </div>
-        </>
+      {!_isModalOpen && (
+        <ModalOverlay
+          toggleModal={toggleModal}
+          staticBackdrop={staticBackdrop}
+        />
       )}
+
+      <div
+        className={` bg-white !rounded-md z-50 transition-all duration-300 absolute shadow-xl min-h-[10rem] left-[50%] translate-x-[-50%] translate-y-[-50%] ${
+          !_isModalOpen ? `top-[50%] opacity-100 visible` : `top-[55%] opacity-0 invisible`
+        } top-[50%] w-[80vw]`}
+      >
+        <ModalHeader id={id} title={title} toggleModal={toggleModal} />
+        <ModalBody>{children}</ModalBody>
+      </div>
     </>
   );
 }
